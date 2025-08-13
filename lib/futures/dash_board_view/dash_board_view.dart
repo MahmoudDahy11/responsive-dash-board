32e9f609e720278437b46db147b28533ee09b379
Mahmoud Dahy <dahym2028@gmail.com>
2025-08-13 13:39:56 +0300

    create dash board view and dash board view body

---
+++ lib/futures/dash_board_view/dash_board_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/futures/dash_board_view/dash_board_view_body.dart';
+
+class DashBoardView extends StatelessWidget {
+  const DashBoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DashBoardViewBody();
+  }
+}
